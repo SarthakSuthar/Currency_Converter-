54f8badd21a86eb3ed983c8a23e261743705746d
SarthakSuthar <115407041+SarthakSuthar@users.noreply.github.com>
2024-02-09 13:37:58 +0530

    Add files via upload

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverter extends StatefulWidget {
+  const CurrencyConverter({super.key});
+
+  @override
+  State createState() => _CurrencyConverterState();
+}
+
+class _CurrencyConverterState extends State {
+  double result = 0;
+  TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "INR ${result != 0 ? result.toStringAsFixed(2) : result.toStringAsFixed(0)}",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "1 USD = 82 INR",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Enter USD",
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(
+                    Icons.monetization_on_outlined,
+                  ),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: const Color.fromRGBO(255, 255, 255, 1),
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 82;
+                  });
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: const Text("Convert"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
